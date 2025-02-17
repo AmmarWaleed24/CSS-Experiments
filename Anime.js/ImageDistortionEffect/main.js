b26c5e9e85d994162a062ee7c46d88657a0d5b23
Ammar Waleed <ongo9972@gmail.com>
2025-02-17 15:18:47 +0200

    Update main.js

--- Anime.js/ImageDistortionEffect/main.js
+++ Anime.js/ImageDistortionEffect/main.js
@@ -1,56 +1,63 @@
+let container = document.querySelector(".container");
 
-let container = document.querySelector('.container');
+for(let i =0; i<=400; i++) {
 
-// Create 400 blocks
-for (let i = 0; i <= 400; i++) {
     let block = document.createElement('div');
-    block.classList.add('block');
+    block.classList.add("block");
     container.appendChild(block);
 }
 
-
 let blocks = document.querySelectorAll(".block");
-  
+//function to start animation
 function startAnimation() {
-let animation = anime.timeline({
-    targets: blocks,
-    easing: 'easeInOutExpo',
-    loop: 1
-    
-});   
-    animation
-    .add({
-        scale: 0,
-        translateX: () => anime.random(-360, 2100),
-        translateY: () => anime.random(-360, 2100),
-        rotate: () => anime.random(-360, 360),
-        duration: () => anime.random(500, 3000)
-      
+
+    let animation = anime.timeline({
+
+        targets:blocks,
+        easing:'easeInOutExpo',
+        loop:1
+
+    }).add({
+
+        scale:0,
+        translateX:()=>{return anime.random(-360, 2100)},
+        translateY:()=>{return anime.random(-360, 2100)},
+        rotate:()=>{return anime.random(-360, -360)},
+        duration:()=>{return anime.random(500, 3000)}
     })
-    
-    
+}
+//function to stop animation
+function stopAnimation() {
+
+    let animation = anime.timeline({
+
+        targets:blocks,
+        easing:'easeInOutExpo',
+        loop:1
+
+    }).add({
+
+        scale:1,
+        translateX:0,
+        translateY:0,
+        rotate:0,
+        duration:()=>{return anime.random(500, 3000)}
+    })
 }
 
-function stopAnimation() {
-    let animation = anime.timeline({
-    targets: blocks,
-    easing: 'easeInOutExpo',
-    loop: 1
-});   
-animation.add({
-    scale: 1,
-    translateX: 0,
-    translateY: 0,
-    rotate: 0,
-    duration: () => anime.random(500, 3000),
-});
-}
+
 container.addEventListener("mouseenter",()=>{
 
+
     startAnimation();
+
+
 })
 
 container.addEventListener("mouseleave",()=>{
 
+
     stopAnimation();
+    
+
 })
